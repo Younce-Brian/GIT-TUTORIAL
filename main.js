eb011650af437002f13d0784f6143da10c09f5e1
Younce-Brian <61626768+Younce-Brian@users.noreply.github.com>
2020-03-01 08:21:54 +0700

    Adding function 2 which would change the h1 color
    Hey Brian-Be-Strong, I recommend you to add my code to your repository, it is a new function for your h1

--- main.js
+++ main.js
@@ -8,4 +8,7 @@ toggleButton.addEventListener('click', (e) => {
 	const h1 = document.querySelector("h1");
 
 	h1.innerHTML = "Hello World"; //Change h1 state
+	
+	//Function 2: Change the h1's word color
+	h1.style.color = "red";
 });
